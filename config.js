5d464aee06be9f352bc12051e1c003d47fd98b3e
shiran_madusha <87171142+Shiranmadusha@users.noreply.github.com>
2023-02-22 21:46:46 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,52 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://api.zahwazein.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://api.zahwazein.xyz': 'Your Key',
+}
+
+// Other
+global.owner = ['94715142059']
+global.premium = ['94715142059']
+global.packname = 'Hisoka Morrow'
+global.author = 'HUNTER-X Bot'
+global.sessionName = 'HUNTER'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Group Admin Special Features!',
+    botAdmin: 'Bots Should Be Admins First!',
+    owner: 'Special Features of Owner Bot',
+    group: 'Features Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'Special Features for Bot Number Users',
+    wait: 'Loading...',
+    endLimit: 'Your Daily Limit Has Been Expired, The Limit Will Reset Every 12 Hours',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/hisoka.jpg')
+global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
